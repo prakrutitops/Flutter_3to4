c55018ac5ee59cebfd4bf7af556f30d166fddfe5
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-02-06 16:04:09 +0530

    Add files via upload

---
+++ Project 1st Module/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:project456/user/splashscreen/splash.dart';
+
+void main()
+{
+  runApp(MaterialApp(home:SplashScreen()));
+}
+
---
+++ Project 1st Module/user/onboarding/widgets/header.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import '../../../constants.dart';
+
+class Header extends StatelessWidget {
+  final VoidCallback onSkip;
+
+  const Header({
+    required this.onSkip,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+          Ink.image(
+            image: AssetImage('images/logo.png'),
+            height: 50,
+            width: 50,
+          ),
+
+        GestureDetector(
+          onTap: onSkip,
+          child: Text(
+            'Skip',
+            style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                  color: kLightGold,
+                ),
+          ),
+        ),
+      ],
+    );
+  }
+}
